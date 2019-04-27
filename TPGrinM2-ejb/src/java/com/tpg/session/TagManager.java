536545c61a5887762f9e538335c8b586061d6526
Mario ANDRIANARILIVA <marioans42@gmail.com>
2019-04-27 20:47:57 +0200

    question: inserer les tags

---
+++ TPGrinM2-ejb/src/java/com/tpg/session/TagManager.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tpg.session;
+
+import com.tpg.entity.Question;
+import com.tpg.entity.QuestionTag;
+import com.tpg.entity.Tag;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author asus
+ */
+@Stateless
+@LocalBean
+public class TagManager {
+    @PersistenceContext(unitName = "TPGrinM2-ejbPU")
+    private EntityManager em;
+    
+    @EJB
+    private QuestionTagManager qtm;
+
+    private void save(Object object) {
+        em.persist(object);
+    }
+    
+    private void update(Object object){
+        em.merge(object);
+    }
+    
+    public void createTag(Tag tag){
+        save(tag);
+    }
+    
+    public void updateTag(Tag tag){
+        update(tag);
+    }
+    
+    public Tag getTag(int id) {  
+        return em.find(Tag.class, id);  
+    }
+    
+    public Tag tagExiste(String label){
+        String requete = "select a from Tag a where upper(a.label) = upper(:label)";
+        Query query = em.createQuery(requete);
+        query.setParameter("label", label);
+        
+        List<Tag> liste = null;
+        liste = query.getResultList(); 
+        if(liste.size() > 0) return liste.get(0);
+        return null;
+    }
+    
+    public void insertQuestionTag(String tagLabel, Question question){
+        String[] label = tagLabel.split("/");
+        for(int i=0; i< label.length;i++){
+            Tag temp = this.tagExiste(label[i]);
+            QuestionTag temp1 = new QuestionTag();
+            if(temp==null){
+                temp = new Tag();
+                temp.setLabel(label[i]);
+                this.createTag(temp);
+                temp1.setTag(temp);
+                temp1.setQuestion(question);
+                qtm.createQuestionTag(temp1);
+            }else {
+                temp1.setTag(temp);
+                temp1.setQuestion(question);
+                qtm.createQuestionTag(temp1);
+            }
+        }
+    }
+}
--- TPGrinM2-war/src/java/com/tpg/managedbean/QuestionBean.java
+++ TPGrinM2-war/src/java/com/tpg/managedbean/QuestionBean.java
@@ -6,9 +6,10 @@
 package com.tpg.managedbean;
 
 import com.tpg.entity.Question;
+import com.tpg.entity.Tag;
 import com.tpg.session.QuestionManager;
-import java.sql.Date;
-import javax.annotation.ManagedBean;
+import com.tpg.session.QuestionTagManager;
+import com.tpg.session.TagManager;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
@@ -23,11 +24,27 @@ public class QuestionBean {
     private String title;
     private String content;
     private String datePost;
+    private String label;
     
     @EJB
     QuestionManager questionManager;
     
+    @EJB
+    QuestionTagManager qtm;
+    
+    @EJB
+    TagManager tagManager;
+    
     private Question question;
+    private Tag tag;
+
+    public Tag getTag() {
+        return tag;
+    }
+
+    public void setTag(Tag tag) {
+        this.tag = tag;
+    }
 
     public Question getQuestion() {
         return question;
@@ -60,6 +77,16 @@ public class QuestionBean {
     public void setQuestion(Question question) {
         this.question = question;
     }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+    
+    
     
     public void saveQuestion() {
         question = new Question();
@@ -68,6 +95,10 @@ public class QuestionBean {
         //question.setDatepost(datePost);
         question.setResolved(false);
         questionManager.createQuestion(question);
+        
+        tag = new Tag();
+        tag.setLabel(label);
+        tagManager.insertQuestionTag(label, question);
     }
     /**
      * Creates a new instance of QuestionBean
