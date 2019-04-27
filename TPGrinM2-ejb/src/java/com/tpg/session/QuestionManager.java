dca786b7484ccd4daf705fa2064211201612aa8c
Mario ANDRIANARILIVA <marioans42@gmail.com>
2019-04-27 21:31:20 +0200

    Question

--- TPGrinM2-ejb/src/java/com/tpg/session/QuestionManager.java
+++ TPGrinM2-ejb/src/java/com/tpg/session/QuestionManager.java
@@ -5,9 +5,13 @@
  */
 package com.tpg.session;
 
+import com.tpg.entity.Member;
 import com.tpg.entity.Question;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -23,6 +27,12 @@ public class QuestionManager {
     // "Insert Code > Add Business Method")
     @PersistenceContext(unitName = "TPGrinM2-ejbPU")
     private EntityManager em;
+    
+    @EJB
+    UserManager userManager;
+    
+    @Inject
+    FacesContext facesContext;
 
     private void save(Object object) {
         em.persist(object);
@@ -33,6 +43,9 @@ public class QuestionManager {
     }
     
     public void createQuestion(Question question){
+        String name = facesContext.getExternalContext().getUserPrincipal().getName();
+        Member user = userManager.findByName(name);
+        question.setUser(user);
         save(question);
     }
     
@@ -43,4 +56,5 @@ public class QuestionManager {
     public Question getQuestion(int id) {  
         return em.find(Question.class, id);  
     }
+    
 }
